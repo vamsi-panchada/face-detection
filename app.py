2c030c2caab4fa055bb5e93b84701f7eafceb969
vamsi panchada <vamsivs58@gmail.com>
2023-04-20 10:41:35 +0530

    new bug resolved

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import cv2
 
 st.title('Face Detection using WebCam')
 run = st.button('RUN')
-Frame_Window = st.image([])
 stop = st.button('STOP')
 if run:
+    # stop = False
+    Frame_Window = st.image([])
     cam = cv2.VideoCapture(0)
     face_cascade = cv2.CascadeClassifier('face.xml')
     while run:
@@ -16,4 +17,14 @@ if run:
             cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         Frame_Window.image(frame)
+        if stop:
+            print('hitting point 1')
+            run = False
+            cam.release()
+            Frame_Window = st.empty()
+    if stop:
+        print('hitting point 2')
+        run = False
+        cam.release()
+        Frame_Window = st.empty()
     cam.release()
